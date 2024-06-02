0c9ebadcb8fe4e1c9a6ab52217e375288c10be34
Ru Chern Chong <hello@ruchern.xyz>
2024-06-02 13:29:12 +0800

    Add file dialog to select an image to check

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
-from PIL import Image
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
 import numpy as np
+from PIL import Image, UnidentifiedImageError
 
 
 def calculate_percent_black(image_path: str) -> int:
-    image = Image.open(image_path)
-    image_grayscale = image.convert("L")
-    image_array = np.array(image_grayscale)
+    try:
+        image = Image.open(image_path)
+        image_grayscale = image.convert("L")
+        image_array = np.array(image_grayscale)
 
-    black_pixels = np.sum(image_array == 0)
-    total_pixels = image_array.size
-    percent_black = black_pixels / total_pixels * 100
+        black_pixels = np.sum(image_array == 0)
+        total_pixels = image_array.size
 
-    print(f"Percentage black: {percent_black}%")
+        return black_pixels / total_pixels * 100
 
-    return percent_black
+    except UnidentifiedImageError:
+        print("The selected file is not a valid image.")
+    except Exception as e:
+        print(f"An error has occurred: {e}")
 
 
 if __name__ == "__main__":
-    calculate_percent_black("OLED Black 4K.png")
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.askopenfilename(
+        title="Select files",
+        filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif *.bmp *.tiff *.tif"),
+                   ("PNG files", "*.png"),
+                   ("JPEG files", "*.jpg *.jpeg"),
+                   ("GIF files", "*.gif"),
+                   ("BMP files", "*.bmp"),
+                   ("TIFF files", "*.tiff *.tif")]
+    )
+
+    if file_path:
+        percent_black = calculate_percent_black(file_path)
+        messagebox.showinfo("Result", f"True OLED: {percent_black:.2f}%")
+    else:
+        print("No file selected")
+
+    root.destroy()
