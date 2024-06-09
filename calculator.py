d96c6fd03bdc62a9c3bf39dcd66e1f83f01e0ee7
Ru Chern Chong <hello@ruchern.xyz>
2024-06-09 19:23:51 +0800

    Remove comments

--- calculator.py
+++ calculator.py
@@ -1,7 +1,3 @@
-# import os.path
-# import tkinter as tk
-# from tkinter import filedialog, messagebox
-
 import numpy as np
 from PIL import Image, UnidentifiedImageError
 
@@ -21,31 +17,3 @@ def calculate_percent_black(image_path: str):
         print("The selected file is not a valid image.")
     except Exception as e:
         print(f"An error has occurred: {e}")
-
-
-# if __name__ == "__main__":
-#     root = tk.Tk()
-#     root.withdraw()
-#
-#     file_path = filedialog.askopenfilename(
-#         title="Select files",
-#         filetypes=[
-#             ("Image files", "*.png *.jpg *.jpeg *.gif *.bmp *.tiff *.tif"),
-#             ("PNG files", "*.png"),
-#             ("JPEG files", "*.jpg *.jpeg"),
-#             ("GIF files", "*.gif"),
-#             ("BMP files", "*.bmp"),
-#             ("TIFF files", "*.tiff *.tif"),
-#         ],
-#     )
-#
-#     if file_path:
-#         percent_black = calculate_percent_black(file_path)
-#         image_name = os.path.basename(file_path)
-#         messagebox.showinfo(
-#             "Result", f"Image: {image_name}\nTrue Black: {percent_black:.2f}%"
-#         )
-#     else:
-#         print("No file selected")
-#
-#     root.destroy()
